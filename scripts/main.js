ef5561366d02f421862d470ee71e1d878cd11bb5
webexpert7111 <tothefuture1117@gmail.com>
2020-07-22 18:50:09 +0800

    add the background images: sea, dock, waterfall, gate, title

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,29 @@
-function start(){
-    
+var preloaderDiv = document.getElementById("preloader-banner-container");
+var staticDiv = document.getElementById("static-background");
+var waterfallDiv1 = document.getElementById("waterfall-1");
+var waterfallDiv2 = document.getElementById("waterfall-2");
+
+function loading(){
+    setTimeout(() => {
+        preloaderDiv.style.top = '0px';
+        preloaderDiv.style.marginTop = '-115px';
+        staticDiv.style.bottom = '0px';
+    }, 1450);
 }
+
+function robby_drop() {
+    setTimeout(() => {
+        
+    }, 1500);
+}
+
+setInterval(() => {
+    if(waterfallDiv2.style.opacity == 1) {
+        waterfallDiv2.style.opacity = 0;
+    }
+    else {
+        waterfallDiv2.style.opacity = 1;
+    }
+}, 500);
+loading();
+robby_drop();
