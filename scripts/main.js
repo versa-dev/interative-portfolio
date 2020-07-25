c67356029a722ca81d31b61188e5db109d6b4997
webexpert7111 <tothefuture1117@gmail.com>
2020-07-26 05:51:35 +0800

    add the vertical movement function

--- scripts/main.js
+++ scripts/main.js
@@ -9,17 +9,23 @@ var robbycontainerDiv = document.getElementById("robby-container");
 var robbyeyecloseDiv = document.getElementById("robby-eyes-close");
 var waterfallDiv = document.getElementById("ground-and-grass-container-2");
 var balloonDiv = document.getElementById("balloon");
+var cloudDiv = document.getElementById("contact-cloud");
+var socialDiv = document.getElementById("social-container");
+var fireworkDiv = document.getElementById("fireworks-container");
+var bannerDiv = document.getElementById("banners-container");
 
 
-balloonDiv.style.left = 7480 - document.body.clientWidth/2 + "px";          // set the balloon position when screen resize
+balloonDiv.style.left = 7480 - document.body.clientWidth/2 + "px";          /* set the balloon position when screen resize */
+cloudDiv.style.bottom = bannerDiv.offsetTop + 50 + "px";
+console.log(cloudDiv.style.bottom, bannerDiv.offsetTop);
 window.addEventListener("resize", balloon_position);
-window.onwheel = horizon;           // add the onwheel event function
+window.onwheel = robby_move;           /* add the onwheel event function */
 
 // waterfall flow animation
 setInterval(() => {
     if(waterfallDiv2.style.opacity == 1) {
         waterfallDiv2.style.opacity = 0;
-    }   else {
+    } else {
         waterfallDiv2.style.opacity = 1;
     }
 }, 500);
@@ -38,7 +44,7 @@ robby_drop();
 
 function balloon_position() {
     balloonDiv.style.left = 7480 + dockDiv.offsetLeft - document.body.clientWidth/2 + "px";
-    console.log(balloonDiv.offsetLeft, robbycontainerDiv.offsetLeft)
+    // console.log(balloonDiv.offsetLeft, robbycontainerDiv.offsetLeft);
 }
 
 // banner animation when preloading
@@ -60,62 +66,67 @@ function robby_drop() {
     }, 2950);
 }
 
-function horizon(event) {  /* onwheel event function for horizontal move */
+function robby_move(event) {  /* onwheel event function for horizontal move */
     
     var splash_left = splashDiv.offsetLeft;
     var dock_left = dockDiv.offsetLeft;
     var waterfall_left = waterfallDiv.offsetLeft;
     var balloon_left = balloonDiv.offsetLeft;
-    var y;
-    
-    if (event.deltaY < 0 && splash_left >= 0){
-        if (splash_left > 0) {
-            balloon_left -= splash_left;
-            waterfall_left -= splash_left;
-            dock_left -= splash_left;
-            splash_left =0;
-        }
-        y = 0;
-    }   else if (dock_left <= ((-7680 + document.body.clientWidth)) && event.deltaY > 0 ) {
-        if (dock_left < (-7680 + document.body.clientWidth)) {
-            var dis = -7680 + document.body.clientWidth - dock_left;
-            dock_left += dis;
-            waterfall_left += dis;
-            balloon_left += dis;
-            splash_left += dis;
-        }
-        y = 0;
-    }   else {
-        y = event.deltaY;
-    }
-    balloon_left -= y;
-    waterfall_left -= y;
-    splash_left -= y;
-    dock_left -= y;
-    splashDiv.style.left = splash_left + "px";
-    dockDiv.style.left = dock_left + "px";    
-    waterfallDiv.style.left = waterfall_left + "px";
-    balloonDiv.style.left = balloon_left + "px";
-    console.log(document.body.clientWidth/2, balloonDiv.offsetLeft+balloonDiv.offsetWidth/2)
-    if (event.deltaY < 0 && Math.abs(document.body.clientWidth/2 - balloonDiv.offsetWidth/2 - balloonDiv.offsetLeft) <= 0.5) {
-        window.onwheel = vertical;
-    }   else {
-        window.onwheel = horizon;
-    }
-}
-
-function vertical(event) {    /* onwheel event function for vertical move */
-    
     var splash_top = splashDiv.offsetTop;
     var dock_top = dockDiv.offsetTop;
     var waterfall_top = waterfallDiv.offsetTop;
     var balloon_top = balloonDiv.offsetTop;
+    var cloud_top = cloudDiv.offsetTop;
+    var banner_top = bannerDiv.offsetTop;
+    var social_top = socialDiv.offsetTop;
+    var firework_top = fireworkDiv.offsetTop;
+    var y;
     var y1;
-
-    if (event.deltaY > 0) {
-        
-    }   else {
-
-    }
     
+    if ((Math.abs(document.body.clientWidth/2 - balloonDiv.offsetWidth/2 - balloonDiv.offsetLeft) <= 1)) {
+        y1 = event.deltaY;
+        splash_top += y1;
+        dock_top += y1;
+        waterfall_top += y1/2;
+        cloud_top += y1;
+        banner_top += y1;
+        social_top += y1;
+        
+        splashDiv.style.top = splash_top + "px";
+        dockDiv.style.top = dock_top + "px";
+        waterfallDiv.style.top = waterfall_top + "px";
+        cloudDiv.style.top = cloud_top + "px";
+        bannerDiv.style.top = banner_top + "px";
+        socialDiv.style.top = social_top + "px";
+    } else {
+        if (event.deltaY < 0 && splash_left >= 0){
+            if (splash_left > 0) {
+                balloon_left -= splash_left;
+                waterfall_left -= splash_left;
+                dock_left -= splash_left;
+                splash_left =0;
+            }
+            y = 0;
+        } else if (dock_left <= ((-7680 + document.body.clientWidth)) && event.deltaY > 0 ) {
+            if (dock_left < (-7680 + document.body.clientWidth)) {
+                var dis = -7680 + document.body.clientWidth - dock_left;
+                dock_left += dis;
+                waterfall_left += dis;
+                balloon_left += dis;
+                splash_left += dis;
+            }
+            y = 0;
+        } else {
+            y = event.deltaY;
+        }
+        balloon_left -= y;
+        waterfall_left -= y;
+        splash_left -= y;
+        dock_left -= y;
+        splashDiv.style.left = splash_left + "px";
+        dockDiv.style.left = dock_left + "px";
+        waterfallDiv.style.left = waterfall_left + "px";
+        balloonDiv.style.left = balloon_left + "px";
+    }
 }
+
